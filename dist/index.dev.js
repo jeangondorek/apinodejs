a369d435e66e64473a3d25d15561ab140190b1e5
jeangondorek <jeangondorek73@gmail.com>
2022-06-19 22:25:05 -0300

    feat: api with mongoosse nodejs

---
+++ dist/index.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var express = required('express');
+var app = express();
+app.use();
+//# sourceMappingURL=index.dev.js.map
---
+++ index.js
@@ -0,0 +1,41 @@
+// config inicial
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+//forma de ler json
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+const personRoutes = require('./routes/personRoutes')
+app.use('/person', personRoutes)
+//rota
+app.get('/',(req, res) => {
+    //mostar req
+
+    res.json({message: 'oi express'})
+})
+
+// mongodb+srv://${DB_USER}:${DB_PASSWD}@apijsjean.pvhda.mongodb.net/?retryWrites=true&w=majority
+
+const DB_USER = 'jeangondorek'
+const DB_PASSWD = encodeURIComponent('k4w8tszi87AyLU4b')
+
+
+//porta
+mongoose
+.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWD}@apijsjean.pvhda.mongodb.net/?retryWrites=true&w=majority`,
+    )
+
+.then(() => {
+    console.log('conectado ao mongo db')
+    app.listen(3000)
+})
+
+.catch((err)=> console.log(err))
